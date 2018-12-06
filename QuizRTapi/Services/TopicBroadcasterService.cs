051caefe4c80b7cf373a2f8adbe92d166a5b06b0
Sai Harshavardhana <ssaiharshavardhana@gmail.com>
2018-12-06 20:06:27 +0530

    added-publish_all_topics-and-publish-specific-topic-to QuizRTRepo.cs-added-delete-all-topics-and-delete-specific-topic-in-topic-broadcaster-service

--- QuizRTapi/Services/TopicBroadcasterService.cs
+++ QuizRTapi/Services/TopicBroadcasterService.cs
@@ -4,6 +4,12 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using RabbitMQ.Client;
 using System.Text;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using QuizRT.Models;
 
 namespace TopicEngine.Services
 {
@@ -11,6 +17,72 @@ namespace TopicEngine.Services
 	{
 		public TopicBroadcaster()
 		{
+		}
+
+		public void Publish_All_Topics_as_deleted(List<string> all_topics)
+		{
+			//IGameContext context = new QuizRTContext();
+			Console.WriteLine("--Broadcasting Topic for deletion--");
+			var factory = new ConnectionFactory() { HostName = "rabbitmq", UserName = "rabbitmq", Password = "rabbitmq" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+					queue: "Delete_Topic",
+					durable: false,
+					exclusive: false,
+					autoDelete: false,
+					arguments: null
+                );
+
+                // String jsonified = JsonConvert.SerializeObject(all_topics);
+                // byte[] body = Encoding.UTF8.GetBytes(jsonified);
+				// var json = JsonConvert.SerializeObject(obj);
+                // return Encoding.ASCII.GetBytes(json);
+                // var body = Encoding.UTF8.GetBytes();
+				for(int i=0;i<all_topics.Count;i++)
+				{
+					PublishTopic_as_deleted(all_topics[i]);
+				}
+
+                // channel.BasicPublish(
+				// 	exchange: "",
+				// 	routingKey: "Delete_Topic",
+				// 	basicProperties: null,
+				// 	body: body
+                // );
+                Console.WriteLine("--{0} All Topics for deletion Queued--");
+            }
+		}
+
+		public void PublishTopic_as_deleted(string Topic)
+		{
+			Console.WriteLine("--Broadcasting Topic for deletion--");
+			var factory = new ConnectionFactory() { HostName = "rabbitmq", UserName = "rabbitmq", Password = "rabbitmq" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+					queue: "Delete_Topic",
+					durable: false,
+					exclusive: false,
+					autoDelete: false,
+					arguments: null
+                );
+
+                // String jsonified = JsonConvert.SerializeObject(new List<string>());
+                // byte[] someBuffer = Encoding.UTF8.GetBytes(jsonified);
+
+                var body = Encoding.UTF8.GetBytes(Topic);
+
+                channel.BasicPublish(
+					exchange: "",
+					routingKey: "Delete_Topic",
+					basicProperties: null,
+					body: body
+                );
+                Console.WriteLine("--{0} Topic for deletion Queued--", Topic);
+            }	
 		}
 
 		public void BroadcastTopics(String Topic)
